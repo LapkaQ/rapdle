f7ea696656d31b7e1ba2f0ed039c5c3c9a0cb363
lapkaQ <wojtekgaw2007@gmail.com>
2024-04-21 21:32:48 +0200

    x

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
   const raperzy = data.map((raper) => (
     <div key={raper.id}>
       <img src={raper.img} alt="" className="h-60" />
-      <h1 className="font-bold text-2xl">{raper.name}</h1> <br />{" "}
+      <h1 className="font-bold text-2xl">{raper.name}</h1> <br />
       <h2>
         Label:{" "}
         <strong>
