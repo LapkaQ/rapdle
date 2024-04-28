e7695fbb33f9a5202418145b3d10f935c2462613
lapkaQ <wojtekgaw2007@gmail.com>
2024-04-28 23:21:05 +0200

    cookie v2

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,14 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <meta charSet="utf-8"></meta>
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.css"
-        />
-      </head>
       <body className="flex h-screen flex-col justify-between">
-        <script type="module" src="cookieconsent-config.js"></script>
         <Header />
         {children}
         <Footer />
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import "vanilla-cookieconsent/dist/cookieconsent.css";
+import * as CookieConsent from "vanilla-cookieconsent";
 import { useState, useEffect } from "react";
 export default function Home() {
   const [data, setData] = useState([]);
@@ -14,6 +16,56 @@ export default function Home() {
           setLoading(false);
         });
     }
+  }, []);
+  useEffect(() => {
+    CookieConsent.run({
+      guiOptions: {
+        consentModal: {
+          layout: "bar inline",
+          position: "bottom",
+          equalWeightButtons: true,
+          flipButtons: false,
+        },
+        preferencesModal: {
+          layout: "box",
+          position: "right",
+          equalWeightButtons: true,
+          flipButtons: false,
+        },
+      },
+      categories: {
+        necessary: {
+          readOnly: true,
+        },
+        functionality: {},
+      },
+      language: {
+        default: "en",
+        autoDetect: "browser",
+        translations: {
+          en: {
+            consentModal: {
+              title: "Cześć podróżniku, nadszedł czas na ciasteczka!",
+              description:
+                "Ciasteczka są niezbędne do zapewnienia prawidłowego działania naszej strony internetowej. Służą one do zapewnienia wydajności oraz funkcjonalności naszego serwisu. Bez tych plików cookie niektóre funkcje strony mogą nie działać poprawnie, co może negatywnie wpłynąć na doświadczenie użytkownika.",
+              closeIconLabel: "",
+              acceptAllBtn: "Zaakceptuj",
+              acceptNecessaryBtn: "Odrzuć",
+            },
+            preferencesModal: {
+              title: "Centrum Preferencji dotyczących Zgody",
+              closeIconLabel: "Zamknij okno modalne",
+              acceptAllBtn: "Zaakceptuj wszystko",
+              acceptNecessaryBtn: "Odrzuć wszystko",
+              savePreferencesBtn: "Zapisz preferencje",
+              serviceCounterLabel: "Usługa|Usługi",
+              sections: [],
+            },
+          },
+        },
+      },
+      disablePageInteraction: true,
+    });
   }, []);
   const thisYear = new Date().getFullYear();
   const raperzy = data.map((raper) => {
