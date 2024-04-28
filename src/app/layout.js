c965cdccca4dac8debf89ddff9dd500c4fc2c6c3
lapkaQ <wojtekgaw2007@gmail.com>
2024-04-28 22:47:02 +0200

    added cookies i essa

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import "./globalicons.css";
+import "./cookieconsent.css";
 import "./blur.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -12,7 +13,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <meta charSet="utf-8"></meta>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.css"
+        />
+      </head>
       <body className="flex h-screen flex-col justify-between">
+        <script type="module" src="cookieconsent-config.js"></script>
         <Header />
         {children}
         <Footer />
